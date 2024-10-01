0e3595daacc293f3c0d8b1755a41ea2fe14a0dfa
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-10-01 14:59:47 +0530

    my first commit

---
+++ hello python.py
@@ -0,0 +1,11 @@
+name=input("Enter your name:")
+roll_number=int(input("enter your roll number:"))
+roll_number=roll_number+1
+CGPA=float(input("enter your CGPA:"))
+percentage_mark=CGPA*10
+print('name of the student=',name)
+print('roll number',roll_number)
+print('CGPA',percentage_mark,"%")
+
+
+
