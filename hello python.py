4b420b9915397d8fdcefea571c60878f16f29a02
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-10-01 15:36:33 +0530

    document header added

--- hello python.py
+++ hello python.py
@@ -1,11 +1,16 @@
-name=input("Enter your name:")
+'''
+python program to get student details
+author:ann mariya
+date:1-10-24
+version:1.0
+'''
+name=input("enter the student name:")
 roll_number=int(input("enter your roll number:"))
 roll_number=roll_number+1
-CGPA=float(input("enter your CGPA:"))
-percentage_mark=CGPA*10
-print('name of the student=',name)
-print('roll number',roll_number)
-print('CGPA',percentage_mark,"%")
-
+cgpa=float(input("enter your cgpa:"))
+percentage=cgpa*10
+print("name of the student:",name)
+print("roll no:",roll_number)
+print("percentage:",percentage,"%")
 
 
